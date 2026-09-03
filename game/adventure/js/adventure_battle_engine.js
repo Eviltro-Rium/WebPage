d55e67dd705d0e1af78d27e0b1b789b66194532c
Eviltro-Rium <2138365644@qq.com>
2026-09-03 23:24:34 +0800

    代码冗余清理.

--- game/adventure/js/adventure_battle_engine.js
+++ game/adventure/js/adventure_battle_engine.js
@@ -313,11 +313,7 @@
     }
 
     _shuffle(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-      }
-      return array;
+      return window.AdventureDeck.shuffle(array);
     }
 
     _refillPile(owner) {
